9e67b693bb13273fdba8bc5648fbbae16d226c59
Mark Protsyuk <protsyukmark@gmail.com>
2025-06-04 13:29:23 -0700

    learned about one line cin inputs

--- IntroToCPP/input-output/main.cpp
+++ IntroToCPP/input-output/main.cpp
@@ -18,10 +18,16 @@ int main() {
     int age;
 
     // Input from the console using cin
-    cout << "Enter your name: ";
-    cin >> name;
-    cout << "Enter your age: ";
-    cin >> age;
+    // cout << "Enter your name: ";
+    // cin >> name;
+    // cout << "Enter your age: ";
+    // cin >> age;
+
+    // We can also use cin to get both name and age in one line
+    cout << "Enter your name and age (separated by a space): ";
+    // You can use >> as many times as needed to read multiple inputs
+    cin >> name >> age;
+
 
     // Output the input values
     cout << "Your name is " << name << " and you are " << age << " years old." << endl;
