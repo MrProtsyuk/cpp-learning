d9b1a54c7d8167c3057f900f10d6395f6579f54c
Mark Protsyuk <protsyukmark@gmail.com>
2025-06-04 12:02:09 -0700

    a simple lecture on comments

--- comments/main.cpp
+++ comments/main.cpp
@@ -1,8 +1,18 @@
+// This brings in the iostream library
 #include <iostream>
 
+// This allows us to use standard namespace features without prefixing with std::
 using namespace std;
 
+// This is a function that prints hello world to the console
 int main() {
     cout << "Hello, World!" << endl;
     return 0;    
 }
+
+// One line comment
+
+/*
+    Multi-line comment
+    This is a simple C++ program that prints "Hello, World!" to the console.
+ */
