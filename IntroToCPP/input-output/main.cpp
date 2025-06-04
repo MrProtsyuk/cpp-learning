53d2a6a11b2610531858d3dbffc4cbc82087a274
Mark Protsyuk <protsyukmark@gmail.com>
2025-06-04 13:26:47 -0700

    created a simple program that using inputs and outputs

--- IntroToCPP/input-output/main.cpp
+++ IntroToCPP/input-output/main.cpp
@@ -1,8 +1,29 @@
+// Input and output is all about reading from and writing to the console.
+
 #include <iostream>
 
 using namespace std;
 
 int main() {
+    // Output a simple message to the console
     cout << "Hello, World!" << endl;
+
+    // Output a formatted message
+    cerr << "This is an error message." << endl;
+
+    // Output a log message
+    clog << "This is a log message." << endl;
+
+    string name;
+    int age;
+
+    // Input from the console using cin
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Enter your age: ";
+    cin >> age;
+
+    // Output the input values
+    cout << "Your name is " << name << " and you are " << age << " years old." << endl;
     return 0;
 }
