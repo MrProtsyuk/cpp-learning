7223441ff875e597f00cce476bdad7c45dbb07ec
Mark Protsyuk <protsyukmark@gmail.com>
2025-06-06 16:12:39 -0700

    created a basic file

---
+++ Var-DataTypes/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    string name;
+    cout << "Enter your name: ";
+    cin >> name;
+    cout << "Hello " + name + " very glad you could join us today." << endl;
+    return 0;
+}
